561a45fecc7d31833def6046eb2498bb922b6bc3
ChaeMendoza <israelmendoza656@outlook.com>
2023-06-21 17:07:17 -0500

    Add the new components in the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-
 import './App.css'
+import ContentGenerator from './components/ContentGenerator'
+import DesignSuggestion from './components/DesignSuggestion'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div className="bg-gray-900 h-screen">
-      <h1 className='text-center text-white text-3xl'>Welcome to DesingWeb APP</h1>
+    <div className='bg-gray-100'>
+      <Header />
+      <ContentGenerator />
+      <h2 className='text-center text-gray-700 text-2xl font-semibold mb-4'>Sugerencia de colores</h2>
+      <DesignSuggestion suggestion='Alguna sugerencia aquí...'/>
+      <Footer />
     </div>
   )
 }
