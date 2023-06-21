e571ec73a55efd356a4512d3387f7fa2f36de3b6
ChaeMendoza <israelmendoza656@outlook.com>
2023-06-21 10:13:57 -0500

    Initial config project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-gray-900 h-screen">
+      <h1 className='text-center text-white text-3xl'>Welcome to DesingWeb APP</h1>
+    </div>
+  )
+}
+
+export default App
